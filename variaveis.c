c943fc7f9db81de018da64e5c9ab824bc1c41152
NicolasLima01 <nicolasrlima@protonmail.com>
2025-08-17 15:28:10 -0300

    feat: entrada de dados

--- variaveis.c
+++ variaveis.c
@@ -3,34 +3,16 @@
 
 // função main
 int main() {
-    #include <stdio.h>
-
-    //especificadores
-
-    // %d: Imprime um inteiro no formato decimal.
-     
-    // %i: Equivalente a %d.
-     
-    // %f: Imprime um número de ponto flutuante no formato padrão.
-     
-    // %e: Imprime um número de ponto flutuante na notação científica.
-     
-    // %c: Imprime um único caractere.
-     
-    // %s: Imprime uma cadeia (string) de caracteres.
-
-    // variaveis
-    int idade = 25; //inteiro
-    float altura = 1.75; //decimal
-    double saldoBancario = 12345.67; //decimal
-    char inicial = 'A'; //caractere
-    char nome[20] = "Bruno"; // cadeia de caracteres (string) 
+    //entrada de valores
  
-    printf("Idade: %d anos\n", idade);
-    printf("Altura: %.2f metros\n", altura);
-    printf("Saldo Bancário: %.2f reais\n", saldoBancario);
-    printf("Inicial do Nome: %c\n", inicial);
-    printf("Nome: %s\n", nome);
-
+    int idade;
+    printf(" \n Digite sua idade: \n");
+    scanf("%d", &idade); //função que le o que usuario escreveu, precisa-se definir o especificador e a variável (&)
+    printf("Sua idade é: %d\n", idade);
     return 0;
+
+    char nome[20] = "sla";
+    printf(" \n Digite seu nome: \n");
+    fgets(nome, 50, stdin); //função que le strings com espaço (50 caracteres)
+    printf("Sua idade é: %d\n", idade);
 }
