ae91855eddcbd317613a30e874e02b77084a43a1
NicolasLima01 <nicolasrlima@protonmail.com>
2025-08-17 15:44:59 -0300

    desafio: cadastro de alunos

---
+++ cadastro_alunos.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+    int idade, matricula;
+    float altura;
+    char nome[50];
+
+    printf("Digite sua idade: \n");
+    scanf("%d", &idade);
+    
+    printf("Digite sua altura: \n");
+    scanf("%f", &altura);
+    
+    printf("Digite seu nome: \n");
+    scanf("%s", &nome);
+    
+    printf("Digite seu matricula: \n");
+    scanf("%d", &matricula);
+
+    printf("Nome do aluno: %s - Matrícula: %d \n", nome, matricula);
+    printf("Idade: %d - Altura: %f", idade, altura);
+}
